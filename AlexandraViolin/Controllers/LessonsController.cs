e7e37f3927ae60d68470e5080a50e6c63f34bdb2
DT <pelopones@mail.ru>
2016-10-02 21:26:12 +0300

    Base

---
+++ AlexandraViolin/Controllers/LessonsController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace AlexandraViolin.Controllers
+{
+    public class LessonsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
---
+++ AlexandraViolin/Controllers/PortfolioController.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Web.Mvc;
+
+
+namespace AlexandraViolin.Controllers
+{
+    
+    
+    public class PortfolioController : Controller
+    {
+        
+        public ActionResult OneColumn()
+        {
+            return View();
+        }
+        
+        public ActionResult SingleItem()
+        {
+            return View();
+        }
+
+        public ActionResult ThreeColumn()
+        {
+            return View();
+        }
+    }
+}
