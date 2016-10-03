d0e487353126698b51259dfd30c07ac212b18069
Yurii <tpmykola@mail.ru>
2016-10-03 12:01:19 +0300

    Topmenu resize onScroll

---
+++ AlexandraViolin/js/violin.js
@@ -0,0 +1,20 @@
+﻿var navigation = document.querySelector('.navbar'),
+	pageHeight = document.documentElement.scrollHeight,
+	offsetWidth = document.documentElement.offsetWidth,
+	viewportHeight = document.documentElement.clientHeight;
+
+window.onscroll = function () {
+    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop,
+        widthLarge = offsetWidth >= 1200 || false;
+    /*
+    if ((scrollTop >= (viewportHeight / 2)) && widthLarge) {
+        navigation.classList.add("navbar-small");
+    }
+    */
+    if (scrollTop >= (viewportHeight - 500)) {
+        navigation.classList.add("navbar-small");
+    }
+    if (scrollTop < (viewportHeight - 500)) {
+        navigation.classList.remove("navbar-small");
+    }
+}
