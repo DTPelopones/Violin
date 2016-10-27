13035a1c43e9aefe94ab63f124bb34355ec228d1
DT <pelopones@mail.ru>
2016-10-27 21:57:49 +0300

    add notfound + hover for afisha

---
+++ AlexandraViolin/Controllers/ErrorController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Violin.Domain;
+
+namespace AlexandraViolin.Controllers
+{
+    public class ErrorController : BaseController
+    {
+        public ActionResult NotFound()
+        {
+            Response.StatusCode = 404;
+            return View();
+        }
+
+        public ActionResult Forbidden()
+        {
+            Response.StatusCode = 403;
+            return View();
+        }
+    }
+}
