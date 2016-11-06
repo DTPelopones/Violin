0313a6a4732d7c7bfe42430b64b522642b3cb5d4
DT <pelopones@mail.ru>
2016-11-06 14:35:13 +0300

    fix bundles + debug=false + EnableOptimizations=true + useCdn

--- AlexandraViolin/App_Start/BundleConfig.cs
+++ AlexandraViolin/App_Start/BundleConfig.cs
@@ -7,64 +7,76 @@ namespace AlexandraViolin
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-1.10.2.min.js"
-                        ));
+            bundles.UseCdn = true;
 
-            bundles.Add(new ScriptBundle("~/bundles/ajax").Include(
-                        "~/Scripts/jquery.unobtrusive-ajax.min.js"
+            BundleTable.Bundles.UseCdn = true;
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery", "https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js")
+                        .Include(
+                        "~/Scripts/jquery-1.7.2.min.js"
                         )); 
+
+            bundles.Add(new ScriptBundle("~/bundles/ajax", "http://ajax.aspnetcdn.com/ajax/mvc/3.0/jquery.unobtrusive-ajax.min.js").Include(
+                        "~/Scripts/jquery.unobtrusive-ajax.min.js" 
+                        ));  
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryvalidate").Include(
                         "~/Scripts/jquery.validate.min.js"
                         )); 
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryvalidateunobstrusive").Include( 
-                        "~/Scripts/jquery.validate.unobstrusive.min.js"
+            bundles.Add(new ScriptBundle("~/bundles/jqueryunobtrusiveajax", "http://ajax.aspnetcdn.com/ajax/mvc/3.0/jquery.unobtrusive-ajax.min.js").Include(
+                        "~/Scripts/jquery.unobtrusive-ajax.min.js"
                         ));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryajaxvalidate").Include( 
-                        "~/Scripts/jquery-1.10.2.min.js",
-                        "~/Scripts/jquery.unobtrusive-ajax.min.js",
-                        "~/Scripts/jquery.validate.min.js",
+            bundles.Add(new ScriptBundle("~/bundles/jqueryvalidateunobtrusive", "http://ajax.aspnetcdn.com/ajax/mvc/5.0/jquery.validate.unobtrusive.min.js").Include(
                         "~/Scripts/jquery.validate.unobtrusive.min.js"
                         ));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrapviolin").Include(
-                        "~/js/bootstrap.min.js",
+            bundles.Add(new ScriptBundle("~/bundles/bootstrapjs", "https://yastatic.net/bootstrap/3.1.1/js/bootstrap.min.js")
+                        .Include(
+                        "~/js/bootstrap.min.js"
+                        )
+                        );
+
+            bundles.Add(new ScriptBundle("~/bundles/violin").Include(
                         "~/js/violin.js"
                         ));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                        "~/js/bootstrap.min.js"
-            ));
 
             bundles.Add(new ScriptBundle("~/bundles/photoswipejs").Include(
                         "~/PhotoSwipe/photoswipe.min.js",
                         "~/PhotoSwipe/photoswipe-ui-default.min.js"
-            ));
+                        ));
 
             bundles.Add(new ScriptBundle("~/bundles/photoswipemanjs").Include(
                     "~/js/photoswipe.js"
-            ));
+                        ));
 
-            bundles.Add(new StyleBundle("~/photoswipecss").Include(
+            StyleBundle photoswipecss = new StyleBundle("~/bundles/photoswipecss");
+
+            photoswipecss.Include(
                       "~/PhotoSwipe/photoswipe.css",
-                      "~/PhotoSwipe/default-skin/default-skin.css"
-                      ));
+                      new CssRewriteUrlTransform()
+                      );
 
-            bundles.Add(new StyleBundle("~/css").Include(
+            photoswipecss.Include(
+                      "~/PhotoSwipe/default-skin/default-skin.css",
+                      new CssRewriteUrlTransform()
+                      );
+
+            bundles.Add(photoswipecss);
+
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
                       "~/css/bootstrap.css",
-                      "~/css/modern-business.css",
-                      "~/css/font-awesome.min.css",
+                      "~/css/modern-business.css", 
                       "~/css/bootstrap-social.css"
                       ));
 
-            bundles.Add(new StyleBundle("~/css/violin").Include(
+            bundles.Add(new StyleBundle("~/bundles/css/violin").Include(
                       "~/css/violin.css"
                       ));
 
-            BundleTable.EnableOptimizations = false;
+            BundleTable.EnableOptimizations = true;
+
         }
     }
 }
